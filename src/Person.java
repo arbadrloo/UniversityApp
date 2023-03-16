00b8787de4594c1ce0439b1df24e13cdd52817e2
Alireza Badrloo <arbadrloo@gmail.com>
2023-03-16 05:27:13 +0330

    updated class "Course" & Person.
    Imported "java.util.*" & changed method "courses" in "Person" and used it in method "constructor".

--- src/Person.java
+++ src/Person.java
@@ -1,3 +1,5 @@
+import java.util.*;
+
 public abstract class Person {
 protected String id;
 protected String name;
@@ -6,12 +8,12 @@ protected ArrayList<Course> courses;
     public Person(String id, String name, ArrayList<Course> courses) {
         this.id = id;
         this.name = name;
-        this.courses = courses;
+        this.courses = new ArrayList<Course>();
     }
     public abstract void addCourse(Course course);
     public abstract void removeCourse(Course course);
     public abstract void setName();
-    public String getId;
+    public  String getId;
     public String getName;
     public Course getCourses;
 
