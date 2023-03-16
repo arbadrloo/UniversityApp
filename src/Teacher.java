c3f60e3a1d05d1c5c92aac93e57739a6c778f1fa
Alireza Badrloo <arbadrloo@gmail.com>
2023-03-16 05:31:04 +0330

    added class "Teacher".
    in this state I just created the class and added its fields.
    class "Teacher" extends "Person".

---
+++ src/Teacher.java
@@ -0,0 +1,23 @@
+import java.util.ArrayList;
+
+public class Teacher extends Person{
+
+    public Teacher(String id, String name, ArrayList<Course> courses) {
+        super(id, name, courses);
+    }
+
+    @Override
+    public void addCourse(Course course) {
+
+    }
+
+    @Override
+    public void removeCourse(Course course) {
+
+    }
+
+    @Override
+    public void setName() {
+
+    }
+}
